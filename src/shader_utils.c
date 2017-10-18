519c9b65d49138fec9d74ce11aaef51814068df6
Nicklas Avén <nicklas.aven@jordogskog.no>
2017-10-18 22:13:36 +0200

    fix license and copyright notice

--- src/shader_utils.c
+++ src/shader_utils.c
@@ -1,4 +1,25 @@
-
+/**********************************************************************
+ *
+ * TileLess
+ *
+ * TileLess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * TileLess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TileLess.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************
+ *
+ * Copyright (C) 2016-2017 Nicklas Avén
+ *
+ ***********************************************************************/
 
 #include "SDL.h"
 #include <stdio.h>
