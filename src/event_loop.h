b3839053dfc2c88530790b707efe17a7d9bc0107
Nicklas Avén <nicklas.aven@jordogskog.no>
2018-02-19 09:27:25 +0100

    change from TileLessMap to TilelessMap

--- src/event_loop.h
+++ src/event_loop.h
@@ -1,19 +1,19 @@
 /**********************************************************************
  *
- * TileLessMap
+ * TilelessMap
  *
- * TileLessMap is free software: you can redistribute it and/or modify
+ * TilelessMap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * TileLessMap is distributed in the hope that it will be useful,
+ * TilelessMap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with TileLessMap.  If not, see <http://www.gnu.org/licenses/>.
+ * along with TilelessMap.  If not, see <http://www.gnu.org/licenses/>.
  *
  **********************************************************************
  *
