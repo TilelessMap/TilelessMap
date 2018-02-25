d34bf97ab3e9cb350bbca9e02469f92db8248076
Nicklas Avén <nicklas.aven@jordogskog.no>
2018-02-25 12:27:08 +0100

    Update build scripts

--- Android/app/src/main/jni/src/Android.mk
+++ Android/app/src/main/jni/src/Android.mk
@@ -21,22 +21,16 @@ LOCAL_CFLAGS += -DSQLITE_ENABLE_RTREE -O3  -funroll-loops
 LOCAL_LDLIBS += -lGLESv1_CM -lGLESv2 -llog
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+$(THE_APP_ROOT)/main.c
 $(THE_APP_ROOT)/event_loop.c \
 $(THE_APP_ROOT)/cleanup.c \
 $(THE_APP_ROOT)/init.c \
-$(THE_APP_ROOT)/read_sld.c \
 $(THE_APP_ROOT)/symbols.c \
 $(THE_APP_ROOT)/pip.c \
 $(THE_APP_ROOT)/info.c \
-$(THE_APP_ROOT)/interface/ui.c \
-$(THE_APP_ROOT)/interface/interface.c \
-$(THE_APP_ROOT)/interface/textbox.c \
-$(THE_APP_ROOT)/interface/radiobutton.c \
-$(THE_APP_ROOT)/interface/table.c \
-$(THE_APP_ROOT)/fonts.c \
-$(THE_APP_ROOT)/reproject.c \
 $(THE_APP_ROOT)/mem.c \
 $(THE_APP_ROOT)/buffer_handling.c \
+$(THE_APP_ROOT)/reproject.c \
 $(THE_APP_ROOT)/gps.c \
 $(THE_APP_ROOT)/label_utils.c \
 $(THE_APP_ROOT)/layer_utils.c \
@@ -44,19 +38,25 @@ $(THE_APP_ROOT)/init_data.c \
 $(THE_APP_ROOT)/linewidth.c \
 $(THE_APP_ROOT)/simple_geometries.c \
 $(THE_APP_ROOT)/log.c \
-$(THE_APP_ROOT)/text.c \
 $(THE_APP_ROOT)/mem_handling.c \
-$(THE_APP_ROOT)/handle_db.c \
 $(THE_APP_ROOT)/shader_utils.c \
 $(THE_APP_ROOT)/utils.c \
 $(THE_APP_ROOT)/getData.c \
-$(THE_APP_ROOT)/eventHandling.c \
-$(THE_APP_ROOT)/twkb.c \
-$(THE_APP_ROOT)/varint.c \
-$(THE_APP_ROOT)/twkb_decode.c \
 $(THE_APP_ROOT)/rendering.c \
-$(THE_APP_ROOT)/touch.c \
-$(THE_APP_ROOT)/main.c
+$(THE_APP_ROOT)/styling/read_sld.c \
+$(THE_APP_ROOT)/text/fonts.c \
+$(THE_APP_ROOT)/text/text.c \
+$(THE_APP_ROOT)/read_data/twkb.c \
+$(THE_APP_ROOT)/read_data/varint.c \
+$(THE_APP_ROOT)/read_data/twkb_decode.c \
+$(THE_APP_ROOT)/handle_input/eventHandling.c \
+$(THE_APP_ROOT)/handle_input/touch.c \
+$(THE_APP_ROOT)/interface/table.c \
+$(THE_APP_ROOT)/interface/button.c \
+$(THE_APP_ROOT)/interface/interface.c \
+$(THE_APP_ROOT)/interface/ui.c \
+$(THE_APP_ROOT)/interface/textbox.c \
+$(THE_APP_ROOT)/interface/radiobutton.c \
 
 
 
